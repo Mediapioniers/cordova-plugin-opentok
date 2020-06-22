06a05060c9ea7b5d7ae11f6c42e7cfbfd1e7605a
Jochum van der Ploeg <jochum@vdploeg.net>
2020-06-22 10:58:49 +0200

    Testing if this works

--- scripts/downloadJSSDK.js
+++ scripts/downloadJSSDK.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var nodeDependencies = require('./downloadNpmDependencies.js');
-    nodeDependencies.install(context).then(() => {
-        var jsSDKVersion = "v2.17.3";
-        var downloadFile = require('./downloadFile.js'),
-            Q = require('q'),
-            deferral = new Q.defer();
-        console.log('Downloading OpenTok JS SDK ' + jsSDKVersion);
-        downloadFile('https://enterprise.opentok.com/' + jsSDKVersion + '/js/opentok.js', './opentok-web.js', function (err) {
-            if (!err) {
-                console.log('Downloaded OpenTok JS SDK ' + jsSDKVersion);
-                deferral.resolve();
-            }
-        });
-        return deferral.promise;
+    var jsSDKVersion = "v2.17.3";
+    var downloadFile = require('./downloadFile.js'),
+        Q = require('q'),
+        deferral = new Q.defer();
+    console.log('Downloading OpenTok JS SDK ' + jsSDKVersion);
+    downloadFile('https://enterprise.opentok.com/' + jsSDKVersion + '/js/opentok.js', './opentok-web.js', function (err) {
+        if (!err) {
+            console.log('Downloaded OpenTok JS SDK ' + jsSDKVersion);
+            deferral.resolve();
+        }
     });
+    return deferral.promise;
 };
