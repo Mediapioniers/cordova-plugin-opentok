a5ed1841cac8474c9f6e687fa15c1bbd42180bf3
Jochum van der Ploeg <jochum@vdploeg.net>
2020-06-24 11:24:50 +0200

    Update

--- src/ios/OpenTokPlugin.m
+++ src/ios/OpenTokPlugin.m
@@ -93,11 +93,9 @@
     // OTSessionSettings
     OTSessionSettings* _sessionSettings = [[OTSessionSettings alloc] init];
     if (whitelist != nil) {
-        NSLog(@"whitelist is not nil %d", whitelist);
         _sessionSettings.ipWhitelist = whitelist;
     }
     if (proxyUrl != [NSNull null]) {
-        NSLog(@"proxyUrl is not NSNull, %s", (NSString *)proxyUrl);
         _sessionSettings.proxyURL = (NSString *)proxyUrl;
     }
 
